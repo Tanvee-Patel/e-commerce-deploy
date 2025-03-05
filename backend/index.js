e3d327a79ca220c3b6fabcd28c51439c0b093497
Tanvee Patel <tanveedpatel8577@gmail.com>
2025-03-05 14:37:06 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -22,6 +23,19 @@ const contactRouter = require('./routes/common/contactRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, '/frontend/dist'))); // Adjust if using Vite
+
+   app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+   });
+} else {
+   app.get('/', (req, res) => {
+       res.send('API is running...');
+   });
+}
 
 mongoose.connect(process.env.MONGO_URI,{
 useNewUrlParser: true,
