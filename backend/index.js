397edffdc33eb4ac38e17e4746e42c91ec9cfc55
Tanvee Patel <tanveedpatel8577@gmail.com>
2025-03-06 16:23:18 +0530

    index.html

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,9 @@ const notificationsRouter = require('./routes/user/notificationRoutes')
 const app = express()
 const PORT = process.env.PORT || 3000; 
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
+app.use(express.static(path.join(__dirname, '../frontend')))
 app.get("*", (req, res) => {
-   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'))
+   res.sendFile(path.join(__dirname, '../frontend', 'index.html'))
 })
 
 mongoose.connect(process.env.MONGO_URI,{
